b795142a4abe8521fccbdb369e176bb6279bd709
Senthil kumaran <159046534+SENTHILKUMARAN10@users.noreply.github.com>
2026-09-10 10:09:18 +0530

    Load mobile side drawer navigation

--- responsive-device.js
+++ responsive-device.js
@@ -13,16 +13,30 @@
       document.head.appendChild(fonts);
       var css=document.createElement('link');
       css.rel='stylesheet';
-      css.href='premium-v5.css?v=20260910-0955';
+      css.href='premium-v5.css?v=20260910-1030';
       css.setAttribute('data-mf-premium','1');
       document.head.appendChild(css);
     }
+    if(!document.querySelector('link[data-mf-mobile-drawer]')){
+      var drawerCss=document.createElement('link');
+      drawerCss.rel='stylesheet';
+      drawerCss.href='mobile-drawer-v1.css?v=20260910-1030';
+      drawerCss.setAttribute('data-mf-mobile-drawer','1');
+      document.head.appendChild(drawerCss);
+    }
     if(!document.querySelector('script[data-mf-account-pages]')){
       var js=document.createElement('script');
-      js.src='account-pages-v1.js?v=20260910-0955';
+      js.src='account-pages-v1.js?v=20260910-1030';
       js.defer=true;
       js.setAttribute('data-mf-account-pages','1');
       document.body.appendChild(js);
+    }
+    if(!document.querySelector('script[data-mf-mobile-drawer]')){
+      var drawerJs=document.createElement('script');
+      drawerJs.src='mobile-drawer-v1.js?v=20260910-1030';
+      drawerJs.defer=true;
+      drawerJs.setAttribute('data-mf-mobile-drawer','1');
+      document.body.appendChild(drawerJs);
     }
   }
   applyDeviceClass();
